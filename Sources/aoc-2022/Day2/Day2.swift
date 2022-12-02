f1dea48971b5eaf2431d7d092cdcd4b5994c2b2c
Marc Kalmes <mkalmes@mkalmes.net>
2022-12-02 09:41:15 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Sources/aoc-2022/Day2/Day2.swift

---
+++ Sources/aoc-2022/Day2/Day2.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+
+public class Day2: Puzzle {
+
+    enum RockPaperScissor: [String, String] {
+        case rock = ["rivalValue": "A", "ownValue": "X", "pointValue": "1"]
+        case paper =
+        case scissor
+    }
+
+}
