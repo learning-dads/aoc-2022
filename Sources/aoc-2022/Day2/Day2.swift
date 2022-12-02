cc6c1c26927b1b6d56e082008e9d76db98925e6c
rd-marc-gidaszewski <marc.gidaszewski@rewe-digital.com>
2022-12-02 10:43:39 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Sources/aoc-2022/Day2/Day2.swift

--- Sources/aoc-2022/Day2/Day2.swift
+++ Sources/aoc-2022/Day2/Day2.swift
@@ -1,11 +1,30 @@
 import Foundation
 
+class rock: RockPaperScissor {
+    var rivalValue = "A"
+    var ownValue = "X"
+    var pointValue = 1
+}
+
+class paper: RockPaperScissor {
+    var rivalValue = "B"
+    var ownValue = "Y"
+    var pointValue = 2
+}
+
+class scissors: RockPaperScissor {
+    var rivalValue = "C"
+    var ownValue = "Z"
+    var pointValue = 3
+}
+
+protocol RockPaperScissor {
+    var rivalValue: String { get }
+    var ownValue: String { get }
+    var pointValue: Int { get }
+}
 
 public class Day2: Puzzle {
-
-    let rock = (rivalValue: "A", ownValue: "X", pointValue: 1)
-    let paper = (rivalValue: "B", ownValue: "Y", pointValue: 2)
-    let scissors = (rivalValue: "C", ownValue: "Z", pointValue: 3)
 
     let winConditions = ["X":"C","Y":"X","Z":"B"]
     let winningPoints = 6
@@ -15,8 +34,12 @@ public class Day2: Puzzle {
         var totalPoints = 0
         newInput.lines.map { input in
             let components = input.components(separatedBy: .whitespaces)
+
+
+            //1. Determin pointValue
             totalPoints += foo(components[1])
 
+            //2. Determin if won or draw
             if components[0] == components[1] {
                 totalPoints += drawPoints
             } else if winConditions[components[1]] == components[0] {
@@ -31,7 +54,7 @@ public class Day2: Puzzle {
         return "fred"
     }
 
-    func foo(_ input: String) -> Int {
+    func foo(_ input: String) -> RockPaperScissor {
         switch input {
         case rock.ownValue:
             return rock.pointValue
@@ -42,6 +65,4 @@ public class Day2: Puzzle {
         default: return 0
         }
     }
-    //1. Deterin pointValue
-    //2. Determin if won or draw
 }
