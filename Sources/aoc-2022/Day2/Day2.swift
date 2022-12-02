30b8032fa78ea81daf3e354d65faa4d2f81d47a8
rd-marc-gidaszewski <marc.gidaszewski@rewe-digital.com>
2022-12-02 09:53:00 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Sources/aoc-2022/Day2/Day2.swift

--- Sources/aoc-2022/Day2/Day2.swift
+++ Sources/aoc-2022/Day2/Day2.swift
@@ -3,10 +3,17 @@ import Foundation
 
 public class Day2: Puzzle {
 
-    enum RockPaperScissor: [String, String] {
-        case rock = ["rivalValue": "A", "ownValue": "X", "pointValue": "1"]
-        case paper =
-        case scissor
-    }
+    let rock = (rivalValue: "A", ownValue: "X", pointValue: 1)
+    let paper = (rivalValue: "B", ownValue: "Y", pointValue: 2)
+    let scissors = (rivalValue: "C", ownValue: "Z", pointValue: 3)
 
+    let winConditions = ["X":"C","Y":"X","Z":"B"]
+    let winningPoints = 6
+    let drawPoints = 3
+
+    override func part1() -> String {
+        
+    }
+    //1. Deterin pointValue
+    //2. Determin if won or draw
 }
